649f676c91f1308aac3ca0a5524bc4183bbc4493
Marcin <wanago.marcin@gmail.com>
2023-03-29 04:25:23 +0200

    test(Counter): add a basic test for the Counter component

---
+++ src/views/Counter/Counter.test.tsx
@@ -0,0 +1,16 @@
+import { render } from '@testing-library/react';
+import { Counter } from '.';
+
+describe('The Counter component', () => {
+  describe('if the button is not clicked', () => {
+    it('should display 0', () => {
+      const counter = render(<Counter />);
+
+      const paragraph = counter.getByText('The number of clicks: 0', {
+        selector: 'p',
+      });
+
+      expect(paragraph).toBeDefined();
+    });
+  });
+});
