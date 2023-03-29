9b5e739e721edbce9590529253a2355f085014b8
Marcin <wanago.marcin@gmail.com>
2023-03-29 04:45:24 +0200

    test(Counter): test clicking on a button

--- src/views/Counter/Counter.test.tsx
+++ src/views/Counter/Counter.test.tsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import { Counter } from '.';
 
 describe('The Counter component', () => {
@@ -7,6 +7,21 @@ describe('The Counter component', () => {
       const counter = render(<Counter />);
 
       const paragraph = counter.getByText('The number of clicks: 0', {
+        selector: 'p',
+      });
+
+      expect(paragraph).toBeDefined();
+    });
+  });
+  describe('if the button clicked once', () => {
+    it('should display 1', async () => {
+      const counter = render(<Counter />);
+
+      const button = counter.getByText('Click', { selector: 'button' });
+
+      fireEvent.click(button);
+
+      const paragraph = await counter.findByText('The number of clicks: 1', {
         selector: 'p',
       });
 
